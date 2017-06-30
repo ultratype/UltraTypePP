0238e02729e05d1f81aadf8193db4b6d9d8b0e54
gotzathe <gotzathe@gmail.com>
2017-06-29 22:30:01 -0500

    Add NTClient

---
+++ src/NTClient.h
@@ -0,0 +1,23 @@
+#include "httplib.h"
+#include "json.hpp"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <uWS/uWS.h>
+using namespace std;
+using namespace httplib;
+using namespace nlohmann;
+using namespace uWS;
+
+class NTClient {
+public:
+	string uname;
+	string pword;
+	NTClient(string username, string password) {
+		uname = username;
+		pword = password;
+	}
+protected:
+	Hub ws;
+};
--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "NTClient.h"
 
 int main(int argc, char** argv) {
 	return 0;
