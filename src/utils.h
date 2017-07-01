1a01da8a7434feac77804bd166d0137d40962142
gotzathe <ultratypebot@gmail.com>
2017-07-01 18:43:53 -0500

    Async setInterval

--- src/utils.h
+++ src/utils.h
@@ -4,6 +4,10 @@
 #include <uWS/uWS.h>
 #include <map>
 #include <algorithm>
+#include <atomic>
+#include <thread>
+#include <functional>
+#include <future>
 
 class Utils {
 public:
@@ -19,5 +23,16 @@ public:
 	    std::vector<std::string> elems;
 	    split(s, delim, elems);
 	    return elems;
+	}
+	template <class F, class... Args>
+	void setInterval(std::atomic_bool& cancelToken,size_t interval,F&& f, Args&&... args){
+	  cancelToken.store(true);
+	  auto cb = std::bind(std::forward<F>(f),std::forward<Args>(args)...);
+	  std::async(std::launch::async,[=,&cancelToken]()mutable{
+	     while (cancelToken.load()){
+	        cb();
+	        std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+	     }
+	  });
 	}
 };
