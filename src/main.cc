e70aa2349b8ddeb4ab816591bd37727147e52cc6
gotzathe <gotzathe@gmail.com>
2017-07-01 11:54:51 -0500

    NitroType is banning my throw away accounts

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,7 @@
 #include "NTClient.h"
 
 int main(int argc, char** argv) {
-	NTClient nclient = NTClient("asd4wderg", "123asd123"); // Throw-away account
+	NTClient nclient = NTClient("gfsiosd", "123asd123"); // Throw-away account
 	cout << "Logging account in...\n";
 	nclient.login();
 	return 0;
