a2efd9a316dafa19cbcc7897b19c2fd7003a3614
gotzathe <ultratypebot@gmail.com>
2017-07-04 21:33:37 -0500

    Add colors.h

---
+++ src/colors.h
@@ -0,0 +1,14 @@
+// Macros that describe ASCII color output for most major console applications
+#ifdef _COLORS_H
+#define _COLORS_H
+
+#define CLR_RED   "\x1B[31m"
+#define CLR_GRN   "\x1B[32m"
+#define CLR_YEL   "\x1B[33m"
+#define CLR_BLU   "\x1B[34m"
+#define CLR_MAG   "\x1B[35m"
+#define CLR_CYN   "\x1B[36m"
+#define CLR_WHT   "\x1B[37m"
+#define CLR_RESET "\x1B[0m"
+
+#endif
