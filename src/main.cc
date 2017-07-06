1f519bf0f38c88e3a754a13383d7d5de6fd230ab
gotzathe <ultratypebot@gmail.com>
2017-07-06 17:57:09 -0500

    Another banned account

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 void initBot() {
 	NTClient nclient = NTClient(220, 0.98);
-	nclient.login("utpp_bot1", "123asd123"); // Throw-away account
+	nclient.login("utpp_bot2", "123asd123"); // Throw-away account
 	nclient.connect();
 }
 int main(int argc, char** argv) {
