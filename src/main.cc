e247ec6aa82f8d64923d8ff0011ea8ac2704a63f
gotzathe <ultratypebot@gmail.com>
2017-07-06 18:26:36 -0500

    New account

--- src/main.cc
+++ src/main.cc
@@ -5,7 +5,7 @@ using namespace std;
 
 void initBot() {
 	NTClient nclient = NTClient(50, 0.92);
-	nclient.login("sfvea2", "123asd123"); // Throw-away account
+	nclient.login("dskfg4", "123asd123"); // Throw-away account
 	nclient.connect();
 }
 int main(int argc, char** argv) {
